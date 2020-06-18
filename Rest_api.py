b622c1b05b7968afda354f64f0735be7830286d9
sameersrivastava13 <sameersrivastava.193@gmail.com>
2020-06-18 23:46:53 +0530

    PUT api request

--- Rest_api.py
+++ Rest_api.py
@@ -16,11 +16,23 @@ def Multiply_num(num):
 languages = [{"name": "Python"}, {"name": "Java"}]
 
 
+@app.route("/lang/display", methods=["GET"])
+def display():
+    return jsonify({"lanaguages": languages})
+
+
 @app.route("/lang", methods=["POST"])
 def add_language():
     language = {"name": request.json["name"]}
     languages.append(language)
     return jsonify({"languages": languages})
+
+
+@app.route("/lang/<string:name>", methods=["PUT"])
+def edit_language(name):
+    langs = [language for language in languages if language["name"] == name]
+    langs[0]["name"] = request.json["name"]
+    return jsonify({"languages": langs[0]})
 
 
 if __name__ == "__main__":
