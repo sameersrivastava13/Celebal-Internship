bff905f3285b50ca76f16f6d273bc8a43a660559
sameersrivastava.193@gmail.com <Sameersrivastava599@gmail.com>
2020-06-23 18:15:49 +0530

    modified meta classs

--- Rabbit_MQ.py
+++ Rabbit_MQ.py
@@ -6,23 +6,34 @@ try:
 except Exception as e:
     print("Some modules are missing {}".format_map(e))
 
-# object oriented program
+# Meta class
+class MetaClass(type):
+    """Singleton Design pattern"""
 
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
+            return cls._instance[cls]
+
+
+# object oriented program
 class RabbitMq(object):
-    def __init__(self, queue='hello'):
-        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+    def __init__(self, queue="hello"):
+        self._connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host="localhost")
+        )
         self._channel = self._connection.channel()
         self.queue = queue
         self._channel.queue_declare(queue=self.queue)
 
     def publish(self, payload={}):
-        self._channel.basic_publish(exchange='',
-                                    routing_key="hello",
-                                    body=str(payload))
+        self._channel.basic_publish(exchange="", routing_key="hello", body=str(payload))
         print("Published...")
         self._connection.close()
 
-if __name__ == '__main__':
-    server = RabbitMq(queue='hello')
-    server.publish(payload={"Data":"Hello"})
 
+if __name__ == "__main__":
+    server = RabbitMq(queue="hello")
+    server.publish(payload={"Data": "Hello"})
