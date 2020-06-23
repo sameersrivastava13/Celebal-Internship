e0ffe31274ff833730f22522c8bce6bb99f1dbf7
sameersrivastava.193@gmail.com <Sameersrivastava599@gmail.com>
2020-06-23 18:58:25 +0530

    changes to sender

--- Rabbit_MQ.py
+++ Rabbit_MQ.py
@@ -17,23 +17,36 @@ class MetaClass(type):
             cls._instance[cls] = super(MetaClass, cls).__call__(*args, **kwargs)
             return cls._instance[cls]
 
+# now am adding a class so that if again i have to change something i can do
+# basically removing the hard coded part
+
+class RabbitmqConfigure(metaclass=MetaClass):
+    def __init__(self,queue='hello',host='localhost', routing_key='hello', exchange=''):
+        self.queue = queue
+        self.host = host
+        self.routing_key = routing_key
+        self.exchange = exchange
+
 
 # object oriented program
-class RabbitMq(object):
-    def __init__(self, queue="hello"):
-        self._connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host="localhost")
-        )
+class RabbitMq():
+    def __init__(self, server):
+        self.server = server
+
+        self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.server.host))
         self._channel = self._connection.channel()
-        self.queue = queue
-        self._channel.queue_declare(queue=self.queue)
+        self._channel.queue_declare(queue=self.server.queue)
 
     def publish(self, payload={}):
-        self._channel.basic_publish(exchange="", routing_key="hello", body=str(payload))
-        print("Published...")
+        self._channel.basic_publish(exchange=self.server.exchange, routing_key=self.server.routing_key,
+                                    body=str(payload))
+
+        print("Published Message : {}".format(payload))
         self._connection.close()
 
 
 if __name__ == "__main__":
-    server = RabbitMq(queue="hello")
-    server.publish(payload={"Data": "Hello"})
+    server = RabbitmqConfigure(queue='hello',host='localhost', routing_key='hello', exchange='')
+    rabbitmq = RabbitMq(server)
+    rabbitmq.publish(payload={"Data":22})
+
