8cf95425574a62f8801e09a1869775e9f6d90e2f
sameersrivastava13 <sameersrivastava.193@gmail.com>
2020-07-18 18:24:23 +0530

    Example custom exception class

---
+++ Custom_Exception_Classes/Custom_Exception.py
@@ -0,0 +1,15 @@
+class MyCustomError(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+
+        if self.message:
+            return 'MyCustomError, {0} '.format(self.message)
+        else:
+            return 'MyCustomError has been raised with no message.'
+
+
